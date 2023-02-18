26492163cc6bb8f1820e7ff6c8b73497a73af252
Beau Raines <braines@aimconsulting.com>
2023-02-18 14:18:00 -0800

    Exports new AzureStorage class

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
+const azureStorage = require("./src/azure")
 const credentials = require("./src/credentials.js");
 const database = require("./src/database");
 const helpers = require("./src/helpers");
 const jira = require("./src/jira");
 
 module.exports = {
+    azureStorage,
     credentials,
     database,
     helpers,
