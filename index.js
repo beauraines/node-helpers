22feaf4a08f57ce80dca5a06e6766a6c3cb52392
= <=>
2023-02-19 09:46:26 -0800

    Change casing on AzureStorage class

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-const azureStorage = require("./src/azure")
+const AzureStorage = require("./src/azure")
 const credentials = require("./src/credentials.js");
 const database = require("./src/database");
 const helpers = require("./src/helpers");
 const jira = require("./src/jira");
 
 module.exports = {
-    azureStorage,
+    AzureStorage,
     credentials,
     database,
     helpers,
