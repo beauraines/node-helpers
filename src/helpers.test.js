74accc3ef2731809987210f39e45d1fd1c71e6ba
Beau Raines <beau.raines@gmail.com>
2023-02-28 18:55:19 -0800

    feat: Adds sparkline helper (#6)
    Adds sparkline using sparkly, including formatted output

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,9 +1,21 @@
 const helper = require('./helpers');
 
-it('should return a title case string',() => {
-    const lowerString = 'a quick brown fox';
-    const titleString = 'A Quick Brown Fox';
+describe('helpers',()=> {
+    it('should return a title case string',() => {
+        const lowerString = 'a quick brown fox';
+        const titleString = 'A Quick Brown Fox';
+    
+        expect(helper.toTitleCase(lowerString)).toBe(titleString);
+    })
 
-    expect(helper.toTitleCase(lowerString)).toBe(titleString);
+    it('should return the correctly sparkline',()=>{
+        const expectedOutput = 'last 30 days [1,5] ▁▂▄▆█ 5'
+        const input = [1,2,3,4,5]
+        const label = 'last 30 days'
+        expect(helper.sparkline(input,label)).toBe(expectedOutput)
+
+    })
+
+
 })
 
